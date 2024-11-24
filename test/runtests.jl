5cffeffed2002277af4760272b1e88e6be32d98f
mwsohn <mwsohn@gmail.com>
2024-11-23 23:46:24 -0500

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,22 @@
+## runtests.jl  
+
+using DataFrames
+using Margins
+using Stella
+using Test
+using CSV
+using GLM
+using CategoricalArrays
+
+# auto.csv
+cars = CSV.read(download("https://raw.githubusercontent.com/sassoftware/sas-viya-programming/refs/heads/master/data/cars.csv"), DataFrame)
+
+# linear regression
+cars.Origin = categorical(cars.Origin);
+descr(cars)
+lm1 = fit(LinearModel, @formula(Invoice ~ Origin + EngineSize + Cylinders + Horsepower + Weight + Length), cars)
+
+# find differences in Invoice price by origin
+Margins.margins(lm1,:Origin)
+
+
